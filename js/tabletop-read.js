d857c1b94a66d85d88284d2068cfaafba5a595f1
aldousmr <m.r.aldous@bham.ac.uk>
2020-02-13 17:51:33 +0000

    Started switching over to GSheets CMS

---
+++ js/tabletop-read.js
@@ -0,0 +1,18 @@
+var publicSpreadsheetUrl = "https://docs.google.com/spreadsheets/d/1TfiakxYziA91JJTFicUi5vYMEKMf1B55HNVFyu4dAAc/pubhtml";
+
+function init() {
+    Tabletop.init( { key: publicSpreadsheetUrl,
+                     callback: showInfo,
+                     simpleSheet: true
+					}
+		         )
+  }
+
+function showInfo(data, tabletop) {
+  data.forEach(function(data) {
+    home_p_2.innerHTML = data.home_p_2;
+	band_blurb.innerHTML = data.band_blurb
+ });
+}
+
+  window.addEventListener('DOMContentLoaded', init)
